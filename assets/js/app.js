cb622a49e31b01a5aafb8cb06c5fdbf984028597
snehaborole5-ui <snehaborole5@gmail.com>
2026-05-10 12:21:23 +0530

    work on templating

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,21 +15,21 @@ const noStdMsg = document.getElementById('noStdMsg')
 let stdArr = [
     {
         fname: 'Jhon',
-        lanme: 'Doe',
+        lname: 'Doe',
         email: 'jd@gmail.com',
         contact: '1234567890',
         stdId: '6edf782c-2b0d-4fc8-b013-5468a45891fb'
     },
     {
         fname: 'May',
-        lanme: 'Doe',
+        lname: 'Doe',
         email: 'may@gmail.com',
         contact: '7895642310',
         stdId: '6789789789c-2b0d-4fc8-b013-5468a45891fb'
     },
     {
         fname: 'June',
-        lanme: 'Doe',
+        lname: 'Doe',
         email: 'june@gmail.com',
         contact: '9876542310',
         stdId: 'ee688e59-7c9e-40dd-9b92-2b69db8f9db7'

7cf2694d21c37dd264694541e144a09cc0f338bf
snehaborole5-ui <snehaborole5@gmail.com>
2026-05-10 11:41:19 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,27 @@
+const cl = console.log;
+
+
+let stdArr = [
+    {
+        fname: 'Jhon',
+        lanme: 'Doe',
+        email: 'jd@gmail.com',
+        contact: '1234567890',
+        stdId: '6edf782c-2b0d-4fc8-b013-5468a45891fb'
+    },
+    {
+        fname: 'May',
+        lanme: 'Doe',
+        email: 'may@gmail.com',
+        contact: '7895642310',
+        stdId: '6789789789c-2b0d-4fc8-b013-5468a45891fb'
+    },
+    {
+        fname: 'June',
+        lanme: 'Doe',
+        email: 'june@gmail.com',
+        contact: '9876542310',
+        stdId: 'ee688e59-7c9e-40dd-9b92-2b69db8f9db7'
+    
+    }
+]
